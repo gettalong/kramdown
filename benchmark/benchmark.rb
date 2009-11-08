83a77a129c02a29faba980b6f557b77bd771f28a
Thomas Leitner <t_leitner@gmx.at>
2009-11-08 18:19:57 +0100

    Corrected spelling of kramdown

--- benchmark/benchmark.rb
+++ benchmark/benchmark.rb
@@ -24,7 +24,7 @@ FILES.each do |file|
   puts
   puts "Test using file #{file} and #{RUNS} runs"
   Benchmark.bmbm do |b|
-    b.report("Kramdown #{Kramdown::VERSION}") { RUNS.times { Kramdown::Document.new(data).to_html } }
+    b.report("kramdown #{Kramdown::VERSION}") { RUNS.times { Kramdown::Document.new(data).to_html } }
     b.report("Maruku #{Maruku::VERSION}") { RUNS.times { Maruku.new(data, :on_error => :ignore).to_html } }
     b.report("BlueFeather #{BlueFeather::VERSION}") { RUNS.times { BlueFeather.parse(data) } }
     b.report("BlueCloth #{BlueCloth::VERSION}") { RUNS.times { BlueCloth.new(data).to_html } }
