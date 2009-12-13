b85972868dd907fd102b665421d51c9bb86c3d86
Thomas Leitner <t_leitner@gmx.at>
2009-12-13 19:42:45 +0100

    Fixed Maruku version number display

--- benchmark/benchmark.rb
+++ benchmark/benchmark.rb
@@ -4,6 +4,7 @@ require 'stringio'
 require 'kramdown'
 require 'bluecloth'
 require 'maruku'
+require 'maruku/version'
 require 'rdiscount'
 require 'bluefeather'
 
@@ -25,7 +26,7 @@ FILES.each do |file|
   puts "Test using file #{file} and #{RUNS} runs"
   Benchmark.bmbm do |b|
     b.report("kramdown #{Kramdown::VERSION}") { RUNS.times { Kramdown::Document.new(data).to_html } }
-    b.report("Maruku #{Maruku::VERSION}") { RUNS.times { Maruku.new(data, :on_error => :ignore).to_html } }
+    b.report("Maruku #{MaRuKu::Version}") { RUNS.times { Maruku.new(data, :on_error => :ignore).to_html } }
     b.report("BlueFeather #{BlueFeather::VERSION}") { RUNS.times { BlueFeather.parse(data) } }
     b.report("BlueCloth #{BlueCloth::VERSION}") { RUNS.times { BlueCloth.new(data).to_html } }
     b.report("RDiscount #{RDiscount::VERSION}") { RUNS.times { RDiscount.new(data).to_html } }
