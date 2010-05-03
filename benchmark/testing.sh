9c3272eaf95e11d655b3f49fe4c097e5b245c949
Thomas Leitner <t_leitner@gmx.at>
2010-05-03 16:22:38 +0200

    Updated testing script to use all available rvm ruby versions

--- benchmark/testing.sh
+++ benchmark/testing.sh
@@ -2,7 +2,7 @@
 
 source ~/.bashrc
 
-for VERSION in 1.8.5 1.8.6 1.8.7 1.9.1 1.9.2 'jruby 1.4.0'; do
+for VERSION in `rvm list strings`; do
 	rvm $VERSION
 	echo $(ruby -v)
 	RUBYOPT=-rubygems rake test
