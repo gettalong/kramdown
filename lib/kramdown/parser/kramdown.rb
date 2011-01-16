0a3fcd72aaffc2b352268b387192c74329614253
Thomas Leitner <t_leitner@gmx.at>
2011-01-16 15:58:04 +0100

    Fixed bug where block HTML line was recognized as table line
    Block HTML blocks have a higher priority now than tables.

--- lib/kramdown/parser/kramdown.rb
+++ lib/kramdown/parser/kramdown.rb
@@ -84,10 +84,10 @@ module Kramdown
         @link_defs = {}
         @footnotes = {}
 
-        @block_parsers = [:blank_line, :codeblock, :codeblock_fenced, :blockquote, :table, :atx_header,
-                          :setext_header, :horizontal_rule, :list, :definition_list, :link_definition, :block_html,
-                          :footnote_definition, :abbrev_definition, :block_extensions, :block_math,
-                          :eob_marker, :paragraph]
+        @block_parsers = [:blank_line, :codeblock, :codeblock_fenced, :blockquote, :atx_header,
+                          :setext_header, :horizontal_rule, :list, :definition_list, :block_html,
+                          :table, :link_definition, :footnote_definition, :abbrev_definition,
+                          :block_extensions, :block_math, :eob_marker, :paragraph]
         @span_parsers =  [:emphasis, :codespan, :autolink, :span_html, :footnote_marker, :link, :smart_quotes, :inline_math,
                          :span_extensions, :html_entity, :typographic_syms, :line_break, :escaped_chars]
 
