1c7c5001c30492f8bc6ed915f90ca352ff1d06c3
Thomas Leitner <t_leitner@gmx.at>
2013-03-09 23:47:02 +0100

    Adapted testing script to allow execution of custom command

--- benchmark/testing.sh
+++ benchmark/testing.sh
@@ -2,8 +2,11 @@
 
 source ~/.bashrc
 
+COMMAND="$@"
+if [[ -z "$COMMAND" ]]; then COMMAND="rake test"; fi
+
 for VERSION in `rvm list strings | sort`; do
 	rvm $VERSION
 	echo $(ruby -v)
-	RUBYOPT=-rubygems rake test
+	RUBYOPT=-rubygems $COMMAND
 done
