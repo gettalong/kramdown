c73dcfe7ab69a7ad95536bbef9df44e0309d2da1
Thomas Leitner <t_leitner@gmx.at>
2013-09-14 09:20:19 +0200

    Fixed GH#77: line break inside inline math statement confused parser

--- lib/kramdown/parser/kramdown/math.rb
+++ lib/kramdown/parser/kramdown/math.rb
@@ -38,7 +38,7 @@ module Kramdown
       define_parser(:block_math, BLOCK_MATH_START)
 
 
-      INLINE_MATH_START = /\$\$(.*?)\$\$/
+      INLINE_MATH_START = /\$\$(.*?)\$\$/m
 
       # Parse the inline math at the current location.
       def parse_inline_math
