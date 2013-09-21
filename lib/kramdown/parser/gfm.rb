0e8b27f1cf57bba8960ed01255b2308f30e12395
Thomas Leitner <t_leitner@gmx.at>
2013-09-21 20:05:26 +0200

    Fixed problem with line breaks in GFM parser
    Line break insertion directly in the paragraph parser didn't work
    correctly because it split up lines into separate :text elements
    which lead to problems parsing multiline span elements.

--- lib/kramdown/parser/gfm.rb
+++ lib/kramdown/parser/gfm.rb
@@ -8,32 +8,39 @@ module Kramdown
 
       def initialize(source, options)
         super
+        @span_parsers.delete(:line_break)
         i = @block_parsers.index(:codeblock_fenced)
         @block_parsers.delete(:codeblock_fenced)
         @block_parsers.insert(i, :codeblock_fenced_gfm)
       end
 
+      def parse
+        super
+        add_hard_line_breaks(@root)
+      end
+
+      def add_hard_line_breaks(element)
+        element.children.map! do |child|
+          if child.type == :text && child.value =~ /\n/
+            children = []
+            lines = child.value.split(/\n(?=.)/)
+            lines.each_with_index do |line, index|
+              children << Element.new(:text, (index > 0 ? "\n#{line}" : line))
+              children << Element.new(:br) if index < lines.size - 1
+            end
+            children
+          elsif child.type == :html_element
+            child
+          else
+            add_hard_line_breaks(child)
+            child
+          end
+        end.flatten!
+      end
+
       FENCED_CODEBLOCK_MATCH = /^(([~`]){3,})\s*?(\w+)?\s*?\n(.*?)^\1\2*\s*?\n/m
 
       define_parser(:codeblock_fenced_gfm, /^[~`]{3,}/, nil, 'parse_codeblock_fenced')
-
-      def parse_paragraph
-        result = @src.scan(PARAGRAPH_MATCH)
-        while !@src.match?(self.class::PARAGRAPH_END)
-          result << @src.scan(PARAGRAPH_MATCH)
-        end
-        result.chomp!
-        unless @tree.children.last && @tree.children.last.type == :p
-          @tree.children << new_block_el(:p)
-        end
-        lines = result.lstrip.split(/\n/)
-        lines.each_with_index do |line, index|
-          @tree.children.last.children << Element.new(@text_type, line) << Element.new(:br) << Element.new(@text_type, "\n")
-        end
-        @tree.children.last.children.pop # added one \n too many
-        @tree.children.last.children.pop # added one :br too many
-        true
-      end
 
     end
   end
