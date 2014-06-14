c8779f41f0eb62c645d2e1fb9c48d4560870019b
Thomas Leitner <t_leitner@gmx.at>
2014-06-14 12:06:35 +0200

    Fixed parsing of math blocks that contain pipe characters

--- lib/kramdown/parser/kramdown.rb
+++ lib/kramdown/parser/kramdown.rb
@@ -74,8 +74,8 @@ module Kramdown
 
         @block_parsers = [:blank_line, :codeblock, :codeblock_fenced, :blockquote, :atx_header,
                           :horizontal_rule, :list, :definition_list, :block_html, :setext_header,
-                          :table, :footnote_definition, :link_definition, :abbrev_definition,
-                          :block_extensions, :block_math, :eob_marker, :paragraph]
+                          :block_math, :table, :footnote_definition, :link_definition, :abbrev_definition,
+                          :block_extensions, :eob_marker, :paragraph]
         @span_parsers =  [:emphasis, :codespan, :autolink, :span_html, :footnote_marker, :link, :smart_quotes, :inline_math,
                          :span_extensions, :html_entity, :typographic_syms, :line_break, :escaped_chars]
 
