7764bee53197936da872e6caabeb10afa39ce090
Thomas Leitner <t_leitner@gmx.at>
2015-02-27 09:56:35 +0100

    Allowing class names to start with a dash in IALs/ALDs
    Fixes #226

--- lib/kramdown/parser/kramdown/extensions.rb
+++ lib/kramdown/parser/kramdown/extensions.rb
@@ -129,7 +129,7 @@ module Kramdown
       ALD_ANY_CHARS = /\\\}|[^\}]/
       ALD_ID_NAME = /\w#{ALD_ID_CHARS}*/
       ALD_TYPE_KEY_VALUE_PAIR = /(#{ALD_ID_NAME})=("|')((?:\\\}|\\\2|[^\}\2])*?)\2/
-      ALD_TYPE_CLASS_NAME = /\.(#{ALD_ID_NAME})/
+      ALD_TYPE_CLASS_NAME = /\.(-?#{ALD_ID_NAME})/
       ALD_TYPE_ID_NAME = /#([A-Za-z][\w:-]*)/
       ALD_TYPE_ID_OR_CLASS = /#{ALD_TYPE_ID_NAME}|#{ALD_TYPE_CLASS_NAME}/
       ALD_TYPE_ID_OR_CLASS_MULTI = /((?:#{ALD_TYPE_ID_NAME}|#{ALD_TYPE_CLASS_NAME})+)/
