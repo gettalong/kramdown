a5f1a55d04ae2d6a4299cc8a7dd022b22fdfffcf
Thomas Leitner <t_leitner@gmx.at>
2015-02-28 13:47:50 +0100

    Block IALs can now be applied to footnote definitions
    The applied attributes are currently not used in any converter
    shipped with kramdown.
    
    Fixes #194

--- lib/kramdown/parser/kramdown/extensions.rb
+++ lib/kramdown/parser/kramdown/extensions.rb
@@ -158,7 +158,7 @@ module Kramdown
           parse_extension_start_tag(:block)
         elsif @src.scan(IAL_BLOCK_START)
           if @tree.children.last && @tree.children.last.type != :blank &&
-              (@tree.children.last.type != :eob || [:link_def, :abbrev_def].include?(@tree.children.last.value))
+              (@tree.children.last.type != :eob || [:link_def, :abbrev_def, :footnote_def].include?(@tree.children.last.value))
             parse_attribute_list(@src[1], @tree.children.last.options[:ial] ||= Utils::OrderedHash.new)
             @tree.children << new_block_el(:eob, :ial) unless @src.check(IAL_BLOCK_START)
           else
