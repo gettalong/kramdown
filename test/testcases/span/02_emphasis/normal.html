gettalong/kramdown

commit c972cc2414296b5dbebb77966ee59b2d212586e0

Fixed problem with underscore emphasis being applied inside a word
Fixes #323

lib/kramdown/parser/kramdown/emphasis.rb
@@ -22,7 +22,7 @@ module Kramdown
         element = (result.length == 2 ? :strong : :em)
         type = result[0..0]
 
-        if (type == '_' && @src.pre_match =~ /[[:alnum:]]\z/) || @src.check(/\s/) ||
+        if (type == '_' && @src.pre_match =~ /[^\s\[*_"']\z/) || @src.check(/\s/) ||
             @tree.type == element || @stack.any? {|el, _| el.type == element}
           add_text(result)
           return

test/testcases/span/02_emphasis/normal.html
@@ -46,3 +46,5 @@ Or that <em>*that</em>* way
 Or that *<em>that*</em> way</p>
 
 <p><a href="http://blah.com/blah_%28">http://blah.com/blah_%28</a></p>
+
+<p><a href="A_-B">A-_B</a></p>

test/testcases/span/02_emphasis/normal.text
@@ -46,3 +46,5 @@ Or that *\*that*\* way
 Or that \**that\** way
 
 [http://blah.com/blah_%28](http://blah.com/blah_%28)
+
+[A-_B](A_-B)

commit 12f49e28f49e3bee7636b3b1ed13f3c0f7cf95fd

Fixed emphasis parsing regression introduced by commit c972cc24142
Fixes #333

lib/kramdown/parser/kramdown/emphasis.rb
@@ -22,7 +22,7 @@ module Kramdown
         element = (result.length == 2 ? :strong : :em)
         type = result[0..0]
 
-        if (type == '_' && @src.pre_match =~ /[^\s\[*_"']\z/) || @src.check(/\s/) ||
+        if (type == '_' && @src.pre_match =~ /[[:alpha:]-]\z/) || @src.check(/\s/) ||
             @tree.type == element || @stack.any? {|el, _| el.type == element}
           add_text(result)
           return

test/testcases/span/02_emphasis/normal.html
@@ -48,3 +48,14 @@ Or that *<em>that*</em> way</p>
 <p><a href="http://blah.com/blah_%28">http://blah.com/blah_%28</a></p>
 
 <p><a href="A_-B">A-_B</a></p>
+
+<ul>
+  <li><em>test</em></li>
+  <li>&#8216;<em>test</em>&#8217;</li>
+  <li>&#8220;<em>test</em>&#8221;</li>
+  <li>(&#8220;<em>test</em>&#8221;)</li>
+  <li>(<em>test</em>)</li>
+  <li>“<em>test</em>”</li>
+  <li>`<em>test</em>&#8217;</li>
+  <li>„<em>test</em>“</li>
+</ul>

test/testcases/span/02_emphasis/normal.options
@@ -1,0 +1,1 @@
+:entity_output: :numeric

test/testcases/span/02_emphasis/normal.text
@@ -48,3 +48,12 @@ Or that \**that\** way
 [http://blah.com/blah_%28](http://blah.com/blah_%28)
 
 [A-_B](A_-B)
+
+- _test_
+- '_test_'
+- "_test_"
+- ("_test_")
+- (_test_)
+- “_test_”
+- \`_test_'
+- „_test_“