f5f25eb3402c960e7083895b8b1a736693bcdda8
Alexey Pokhozhaev <poporul@gmail.com>
2016-08-10 12:53:19 +0300

    Allow to convert tex to svg with mathjaxnode

--- lib/kramdown/converter/math_engine/mathjaxnode.rb
+++ lib/kramdown/converter/math_engine/mathjaxnode.rb
@@ -11,6 +11,7 @@ module Kramdown::Converter::MathEngine
 
   # Uses the mathjax-node library for converting math formulas to MathML.
   module MathjaxNode
+    VALID_FORMATS = %w(svg mml)
 
     # MathjaxNode is available if this constant is +true+.
     AVAILABLE = RUBY_VERSION >= '1.9' && begin
@@ -28,8 +29,10 @@ module Kramdown::Converter::MathEngine
         npm = %x{npm --depth=1 list mathjax-node 2>&1}
       end
 
-      T2MPATH = File.join(npm.lines.first.strip, "node_modules/mathjax-node/bin/tex2mml")
-      /mathjax-node@/ === npm.lines.drop(1).join("\n") && File.exist?(T2MPATH)
+      MML_BIN = File.join(npm.lines.first.strip, "node_modules/mathjax-node/bin/tex2mml")
+      SVG_BIN = File.join(npm.lines.first.strip, "node_modules/mathjax-node/bin/tex2svg")
+
+      /mathjax-node@/ === npm.lines.drop(1).join("\n") && File.exist?(MML_BIN) && File.exist?(SVG_BIN)
     rescue
       false
     end
@@ -37,20 +40,37 @@ module Kramdown::Converter::MathEngine
     def self.call(converter, el, opts)
       type = el.options[:category]
 
-      cmd = [T2MPATH]
+      format = converter.options[:math_engine_opts][:format].to_s
+      format = VALID_FORMATS.include?(format) ? format : 'mml'
+
+      cmd = []
+      cmd << const_get("#{format.upcase}_BIN")
       cmd << "--inline" unless type == :block
-      cmd << "--semantics" if converter.options[:math_engine_opts][:semantics] == true
-      cmd << "--notexhints" if converter.options[:math_engine_opts][:texhints] == false
+
+      if format == 'mml'
+        cmd << "--semantics" if converter.options[:math_engine_opts][:semantics] == true
+        cmd << "--notexhints" if converter.options[:math_engine_opts][:texhints] == false
+      end
+
       result = IO.popen(cmd << el.value).read.strip
 
-      attr = el.attr.dup
-      attr.delete('xmlns')
-      attr.delete('display')
-      result.insert("<math".length, converter.html_attributes(attr))
+      if format == 'mml'
+        attr = el.attr.dup
+        attr.delete('xmlns')
+        attr.delete('display')
+        result.insert("<math".length, converter.html_attributes(attr))
+      end
+
+      if format == 'svg'
+        if type == :block
+          result.insert(0, '<p>')
+          result.insert(result.length, '</p>')
+          result.insert('<p'.length, converter.html_attributes(el.attr.dup))
+        end
+      end
 
       (type == :block ? "#{' '*opts[:indent]}#{result}\n" : result)
     end
-
   end
 
 end
