25a4245ffdc32de831b5e73632518fb52add2ec3
Thomas Leitner <t_leitner@gmx.at>
2017-08-27 09:59:15 +0200

    Fix Ruby warning

--- lib/kramdown/document.rb
+++ lib/kramdown/document.rb
@@ -53,7 +53,7 @@ module Kramdown
     unless defined?(@@data_dir)
       require 'rbconfig'
       @@data_dir = File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'data', 'kramdown'))
-      @@data_dir = File.expand_path(File.join(RbConfig::CONFIG["datadir"], "kramdown")) if !File.exists?(@@data_dir)
+      @@data_dir = File.expand_path(File.join(RbConfig::CONFIG["datadir"], "kramdown")) if !File.exist?(@@data_dir)
       raise "kramdown data directory not found! This is a bug, please report it!" unless File.directory?(@@data_dir)
     end
     @@data_dir
