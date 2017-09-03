3c9e8df645ace36932a9f3449f27cd175081202e
Thomas Leitner <t_leitner@gmx.at>
2017-09-03 16:59:25 +0200

    Fix problem with brackets in LaTeX output
    Fixes #414

--- lib/kramdown/converter/latex.rb
+++ lib/kramdown/converter/latex.rb
@@ -137,7 +137,7 @@ module Kramdown
       end
 
       def convert_li(el, opts)
-        "\\item #{latex_link_target(el, true)}#{inner(el, opts).sub(/\n+\Z/, '')}\n"
+        "\\item{} #{latex_link_target(el, true)}#{inner(el, opts).sub(/\n+\Z/, '')}\n"
       end
 
       def convert_dt(el, opts)
@@ -598,7 +598,9 @@ module Kramdown
         "~"  => "\\ensuremath{\\sim}",
         "|"  => "\\textbar{}",
         "<"  => "\\textless{}",
-        ">"  => "\\textgreater{}"
+        ">"  => "\\textgreater{}",
+        "["  => "{[}",
+        "]"  => "{]}",
       }.merge(Hash[*("{}$%&_#".scan(/./).map {|c| [c, "\\#{c}"]}.flatten)]) # :nodoc:
       ESCAPE_RE = Regexp.union(*ESCAPE_MAP.collect {|k,v| k}) # :nodoc:
 
