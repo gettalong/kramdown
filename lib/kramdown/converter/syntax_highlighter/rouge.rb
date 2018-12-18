857733e8c1d8f65b0feb451fcce2e05d2c50a551
Ashwin Maroli <ashmaroli@gmail.com>
2018-12-18 18:30:17 +0530

    Add data attribute to code highlighted by Rouge

--- lib/kramdown/converter/syntax_highlighter/rouge.rb
+++ lib/kramdown/converter/syntax_highlighter/rouge.rb
@@ -28,7 +28,9 @@ module Kramdown::Converter::SyntaxHighlighter
       return nil if opts[:disable] || !lexer
       opts[:css_class] ||= 'highlight' # For backward compatibility when using Rouge 2.0
       formatter = formatter_class(opts).new(opts)
-      formatter.format(lexer.lex(text))
+      formatter.format(lexer.lex(text)).sub(
+        '<code>', "<code data-lang=\"#{lang}\">"
+      )
     end
 
     def self.options(converter, type)
