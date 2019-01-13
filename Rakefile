0a72d04c3f33bbaed90a65c328779d21a34cb7f4
Thomas Leitner <t_leitner@gmx.at>
2019-01-13 12:10:30 +0100

    Update copyright year

--- Rakefile
+++ Rakefile
@@ -236,7 +236,7 @@ EOF
   COPYRIGHT=<<EOF
 #
 #--
-# Copyright (C) 2009-2016 Thomas Leitner <t_leitner@gmx.at>
+# Copyright (C) 2009-2019 Thomas Leitner <t_leitner@gmx.at>
 #
 # This file is part of kramdown which is licensed under the MIT.
 #++
--- lib/kramdown/utils/lru_cache.rb
+++ lib/kramdown/utils/lru_cache.rb
@@ -1,10 +1,11 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 #
 #--
-# Copyright (C) 2014-2017 Thomas Leitner <t_leitner@gmx.at>
+# Copyright (C) 2009-2019 Thomas Leitner <t_leitner@gmx.at>
 #
 # This file is part of kramdown which is licensed under the MIT.
 #++
+#
 
 module Kramdown
   module Utils
