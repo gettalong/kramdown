gettalong/kramdown

commit edbd1fdfec7a6bb7fdcaddac52b4db09aa3b081f

Three fixes regarding entities in LaTeX converter
- Fixed unnecessary string creation while constructing converstion table
- Added helper method for converting an entity to LaTeX
- Removed unnecessary braces from two entity definitions

lib/kramdown/converter/latex.rb
@@ -390,8 +390,8 @@ module Kramdown
         34 => ['"'],
         39 => ['\''],
         169 => ['\copyright'],
-        60 => ['\textless{}'],
-        62 => ['\textgreater{}'],
+        60 => ['\textless'],
+        62 => ['\textgreater'],
         338 => ['\OE'],
         339 => ['\oe'],
         352 => ['\v{S}'],
@@ -510,17 +510,21 @@ module Kramdown
         8194 => ['\hskip .5em\relax'],
         8195 => ['\quad'],
       } # :nodoc:
-      ENTITY_CONV_TABLE.each {|k,v| ENTITY_CONV_TABLE[k] = v.unshift(v.shift + '{}')}
+      ENTITY_CONV_TABLE.each {|k,v| ENTITY_CONV_TABLE[k][0].insert(-1, '{}')}
 
-      def convert_entity(el, opts)
-        text, package = ENTITY_CONV_TABLE[el.value.code_point]
+      def entity_to_latex(entity)
+        text, package = ENTITY_CONV_TABLE[entity.code_point]
         if text
           @data[:packages] << package if package
           text
         else
-          warning("Couldn't find entity in substitution table!")
+          warning("Couldn't find entity with code #{entity.code_point} in substitution table!")
           ''
         end
+      end
+
+      def convert_entity(el, opts)
+        entity_to_latex(el.value)
       end
 
       TYPOGRAPHIC_SYMS = {

commit c1a9c12fce20fdfa4daa2e1e123ac2faa7b26c67

fix: fix LaTeX entity conversion group location
This correctly places the `{}` group after the LaTeX commands used for
conversion, rather than before. It appears this has been broken since
edbd1fdf.

Also updates the span/text_substitutions/entities test to include
`&lt;span&gt;`, which will be converted by the old converter to
`{}\textlessspan{}\textgreater`, which should instead be
`\textless{}span\textgreater{}`. The former will fail to compile, and should
throw an error.

lib/kramdown/converter/latex.rb
@@ -517,7 +517,7 @@ module Kramdown
         8194 => ['\hskip .5em\relax'],
         8195 => ['\quad'],
       } # :nodoc:
-      ENTITY_CONV_TABLE.each_value {|v| v[0] = "{}#{v[0]}" }
+      ENTITY_CONV_TABLE.each_value {|v| v[0] = "#{v[0]}{}" }
 
       def entity_to_latex(entity)
         text, package = ENTITY_CONV_TABLE[entity.code_point]

test/testcases/span/text_substitutions/entities.html
@@ -3,4 +3,4 @@ As well \&amp; as this. Some &#343; other
 values may &#xAF; may also show but
 not st. like &amp;#xYZ;.</p>
 
-<p>This is BS&amp;T; done!</p>
+<p>This &lt;span&gt; is BS&amp;T; done!</p>

test/testcases/span/text_substitutions/entities.text
@@ -3,4 +3,4 @@ As well \& as this. Some &#343; other
 values may &#xAF; may also show but
 not st. like &#xYZ;.
 
-This is BS&T; done!
+This &lt;span&gt; is BS&T; done!